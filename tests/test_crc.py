845ef4642d51d4d52680749c7cfb30e6fa2171a6
Diony Rosa <dhrosa@gmail.com>
2024-06-07 16:17:10 -0400

    Initial commit; CRC implementation

---
+++ tests/test_crc.py
@@ -0,0 +1,14 @@
+from collections.abc import Iterable
+
+from motor_tuner.crc import checksum_bytes
+
+
+def checksum_hex(data: Iterable[int]) -> str:
+    return checksum_bytes(data).hex(" ")
+
+
+def test_crc() -> None:
+    assert checksum_hex([0]) == "bf 40"
+    assert checksum_hex([1]) == "7e 80"
+    assert checksum_hex([0, 1]) == "c0 70"
+    assert checksum_hex([0, 1, 2, 3, 4]) == "85 0f"
